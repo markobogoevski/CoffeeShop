bb3a22554fc89c1e2922034f5dee47c942de6664
Марко Богоевски <markobogoevski@gmail.com>
2020-09-13 18:37:07 +0200

    commit

---
+++ CoffeeShop/Migrations/202009121449366_init2.cs
@@ -0,0 +1,18 @@
+﻿namespace CoffeeShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.CoffeeModels", "ProductionPrice", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.CoffeeModels", "ProductionPrice");
+        }
+    }
+}
---
+++ CoffeeShop/Migrations/202009121739533_SIZE.cs
@@ -0,0 +1,20 @@
+﻿namespace CoffeeShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SIZE : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.OrderItemModels", "CoffeeSize", c => c.String());
+            DropColumn("dbo.CoffeeModels", "Size");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.CoffeeModels", "Size", c => c.String(nullable: false));
+            DropColumn("dbo.OrderItemModels", "CoffeeSize");
+        }
+    }
+}
